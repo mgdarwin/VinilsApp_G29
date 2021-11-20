0765b0330e65ac15adbb287fd080aceb108087fa
rarangogi <r.arango@uniandes.edu.co>
2021-11-20 16:26:25 -0500

    create the musician model

---
+++ app/src/main/java/com/example/vinyls_jetpack_application/models/Musician.kt
@@ -0,0 +1,9 @@
+package com.example.vinyls_jetpack_application.models
+
+data class Musician (
+    val musicianId:Int,
+    val name:String,
+    val image:String,
+    val description:String,
+    val birthDate:String
+)
